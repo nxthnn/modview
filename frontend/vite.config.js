53875a779eecaf5f9934aa0e261a1ffe759a4f9a
Nathan <n.morgan@rgu.ac.uk>
2026-04-20 13:59:19 +0100

    config chance

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: true,
+    port: 8080,
+    strictPort: true,
+    allowedHosts: [
+      'raymondjuliet-igloosierra-8080.codio.io'
+    ]
+  }
+})
